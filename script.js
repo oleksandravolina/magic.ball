0f7275a09cec91ca10a9b3790dc5c6541089b305
oleksandravolina <volinaoleksandra@gmail.com>
2026-03-07 19:13:23 +0100

    Fix answer display and add new responses

--- script.js
+++ script.js
@@ -4,14 +4,28 @@ const answers = [
 "Very likely",
 "Ask later",
 "Definitely",
-"Uncertain",
-"The stars say yes"
+"The stars say yes",
+"Uncertain"
 ];
 
 function getAnswer(){
 
+const answerBox = document.getElementById("answer");
+
+answerBox.style.opacity = 0;
+answerBox.innerText = "";
+
+document.querySelector(".container").classList.add("shake");
+
+setTimeout(()=>{
+
 const random = Math.floor(Math.random()*answers.length);
 
-document.getElementById("answer").innerText = answers[random];
+answerBox.innerText = answers[random];
+answerBox.style.opacity = 1;
+
+document.querySelector(".container").classList.remove("shake");
+
+},800);
 
 }
